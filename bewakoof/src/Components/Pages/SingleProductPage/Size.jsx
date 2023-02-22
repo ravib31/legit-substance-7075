68cc3dcd54716003428aa01c09f2dc58b66b2b1c
Sunil Chaudhary <sunilchaudhary7789@gmail.com>
2023-02-22 23:18:54 +0530

    added singleProductPage 60%

---
+++ bewakoof/src/Components/Pages/SingleProductPage/Size.jsx
@@ -0,0 +1,12 @@
+import { Box, Button } from '@chakra-ui/react'
+import React from 'react'
+
+const Size = ({children}) => {
+  return (
+    <div>
+        <Button>{children}</Button>
+    </div>
+  )
+}
+
+export default Size

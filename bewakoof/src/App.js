463bb2e1fb1b5a75c909ae291e666eb34ee12b04
shreshthkr <shreshthkumar6@gmail.com>
2023-02-25 22:13:54 +0530

    adedd

--- bewakoof/src/App.js
+++ bewakoof/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import { AllRoutes } from './AllRoutes/AllRoutes';
+import "./App.css";
+import { AllRoutes } from "./AllRoutes/AllRoutes";
+import { Navbar } from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-
-    <AllRoutes/>
+      <Navbar />
+      <AllRoutes />
     </div>
   );
 }

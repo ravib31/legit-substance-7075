ae84a2cf0ddc24468ec07d0c853b54cd6624828a
Sunil Chaudhary <sunilchaudhary7789@gmail.com>
2023-05-28 12:15:59 +0530

    features:completed ui of signup page

--- bewakoof/frontend/src/Pages/SingupPage/Signup.jsx
+++ bewakoof/frontend/src/Pages/SingupPage/Signup.jsx
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Signup.module.css";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
 import {
   Box,
+  Button,
   FormControl,
   Heading,
   Image,
   Input,
   InputGroup,
   InputLeftAddon,
+  InputRightElement,
+  Link,
   SimpleGrid,
+  Stack,
   Text,
 } from "@chakra-ui/react";
 
-import { FaUserTie ,} from 'react-icons/fa';
-import { MdEmail ,} from 'react-icons/md';
-import { BsTelephoneFill ,} from 'react-icons/bs';
-import { RiLockPasswordFill ,} from 'react-icons/ri';
+import { FaUserTie } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { BsTelephoneFill } from "react-icons/bs";
+import { RiLockPasswordFill } from "react-icons/ri";
 import SocialMedia from "./SocialMedia";
 
 const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div>
-      <main className={styles.main}>
+      <SimpleGrid
+        className={styles.main}
+        columns={[1, 2, 2]}
+        w={"80%"}
+        margin={"auto"}
+        gap={"10%"}
+        mb={"100px"}
+        mt={"10px"}
+      >
         <div className={styles.leftside}>
           <img
             src="https://images.bewakoof.com/web/desktop-sign-up-banner--1623760676.png"
@@ -32,29 +46,63 @@ const Signup = () => {
         <div>
           <header>
             <p>Signup</p>
-            <>Hi new buddy, let's get you started with the bewakoofi!</>
+            <p>Hi new buddy, let's get you started with the bewakoofi!</p>
           </header>
-          <form action="" >
-          <InputGroup >
-              <InputLeftAddon children=<FaUserTie/> />
-              <Input  type='text' placeholder=' Name' variant={"flushed"} />
+          <form action="">
+            <InputGroup mb={"2"}>
+              <InputLeftAddon children=<FaUserTie /> />
+              <Input type="text" placeholder=" Name" variant={"flushed"} />
             </InputGroup>
-            <InputGroup>
-              <InputLeftAddon children=<BsTelephoneFill/> />
-              <Input type='number' placeholder=' Number' variant={"flushed"} />
+            <InputGroup mb={"2"}>
+              <InputLeftAddon children=<BsTelephoneFill /> />
+              <Input type="number" placeholder=" Number" variant={"flushed"} />
             </InputGroup>
-            <InputGroup>
-              <InputLeftAddon children=<MdEmail/> />
-              <Input type='email' placeholder=' Email' variant={"flushed"} />
+            <InputGroup mb={"2"} >
+              <InputLeftAddon children=<MdEmail /> />
+              <Input  type="email" placeholder=" Email" variant={"flushed"} />
             </InputGroup>
-            <InputGroup>
-              <InputLeftAddon children=<RiLockPasswordFill/> />
-              <Input type='number' placeholder=' Password' variant={"flushed"} />
+            <InputGroup mb={"2"} >
+              <InputLeftAddon children=<RiLockPasswordFill /> />
+              <Input
+                type={showPassword ? "text" : "password"}
+                placeholder=" Password"
+                variant={"flushed"}
+              />
+              <InputRightElement h={"full"}>
+                <Button
+                  variant={"ghost"}
+                  onClick={() =>
+                    setShowPassword((showPassword) => !showPassword)
+                  }
+                >
+                  {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                </Button>
+              </InputRightElement>
             </InputGroup>
+            <Stack spacing={10} pt={2}>
+              <Button
+              
+                loadingText="Submitting"
+                size="lg"
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                Sign up
+              </Button>
+            </Stack>
+            
           </form>
-          <SocialMedia/>
+          <Stack pt={6}>
+              <Text align={"center"}>
+                Already a user? <Link color={"blue.400"}>Login</Link>
+              </Text>
+            </Stack>
+          <SocialMedia />
         </div>
-      </main>
+      </SimpleGrid>
     </div>
   );
 };
--- bewakoof/frontend/src/Pages/SingupPage/SocialMedia.jsx
+++ bewakoof/frontend/src/Pages/SingupPage/SocialMedia.jsx
@@ -1,43 +1,27 @@
-import { FcGoogle } from 'react-icons/fc';
-import { FaFacebook } from 'react-icons/fa';
-import { SiLinkedin, SiMessenger } from 'react-icons/si';
-import { Box, Button, Center, Stack, Text } from '@chakra-ui/react';
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
 
-const SocialMedia=()=> {
+import {  Button, Center, Stack, Text } from "@chakra-ui/react";
+
+const SocialMedia = () => {
   return (
     <Center p={8}>
-      <Stack spacing={2} align={'center'} maxW={'md'} w={'full'}>
-        {/* Facebook */}
-        <Button w={'full'} colorScheme={'facebook'} leftIcon={<FaFacebook />}>
-          <Center>
-            <Text>Continue with Facebook</Text>
-          </Center>
-        </Button>
-
+      <Stack spacing={2} align={"center"} maxW={"md"} w={"full"}>
         {/* Google */}
-        <Button w={'full'} variant={'outline'} leftIcon={<FcGoogle />}>
+        <Button w={"full"} variant={"outline"} leftIcon={<FcGoogle />}>
           <Center>
             <Text>Sign in with Google</Text>
           </Center>
         </Button>
-
-        {/* LinkedIn */}
-        <Button w={'full'} colorScheme={'messenger'} leftIcon={<SiLinkedin />}>
+        {/* Facebook */}
+        <Button w={"full"} colorScheme={"facebook"} leftIcon={<FaFacebook />}>
           <Center>
-            <Text>Send to Linkedin</Text>
-          </Center>
-        </Button>
-
-        {/* Messenger */}
-        <Button w={'full'} colorScheme={'messenger'} leftIcon={<SiMessenger />}>
-          <Center>
-            <Text>Send to Messenger</Text>
+            <Text>Continue with Facebook</Text>
           </Center>
         </Button>
       </Stack>
     </Center>
   );
-}
-
+};
 
 export default SocialMedia;
